b4b82488e708607f0a4d61ae33235abd3af053aa
aaditamanugraha <adhityaaditaman@gmail.com>
2024-01-08 20:11:48 +0800

    Generate random story by clicking the trigger button

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
-
 const customName = document.getElementById("customname");
 const randomize = document.querySelector(".randomize");
 const story = document.querySelector(".story");
@@ -9,27 +7,29 @@ function randomValueFromArray(array) {
   return array[random];
 }
 
-// 2. RAW TEXT STRINGS
+let characterName = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+let storyLocation = ["the soup kitchen", "Disneyland", "the White House"];
+let storyPlot = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and crawled away",
+];
 
-// It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
-
-// the soup kitchen
-// Disneyland
-// the White House
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and crawled away
-
-// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+let sillyStory =
+  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
 randomize.addEventListener("click", result);
 
 function result() {
+  let nameItem = randomValueFromArray(characterName);
+  let locationItem = randomValueFromArray(storyLocation);
+  let plotItem = randomValueFromArray(storyPlot);
+
+  let newStory = sillyStory
+    .replaceAll(":insertx:", nameItem)
+    .replaceAll(":inserty:", locationItem)
+    .replaceAll(":insertz:", plotItem);
+
   if (customName.value !== "") {
     const name = customName.value;
   }
@@ -39,6 +39,6 @@ function result() {
     const temperature = Math.round(94);
   }
 
-  story.textContent = "";
+  story.textContent = newStory;
   story.style.visibility = "visible";
 }
